8fc70fd50a8df0c2786879c408504144fb98760f
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-06-29 14:44:40 -0300

    package.json configurado servidor restartando automaticamente

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.get("/", function(req, res) {
-    return res.send("Hello World! Jeff");
+    return res.send("Hello World! Jeff and julian");
 });
 
 server.listen(5000, function() {
