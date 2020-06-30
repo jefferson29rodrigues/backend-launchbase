0d7a7d91cba8800e3300831814c7936ddb24a6bb
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-06-30 20:09:57 -0300

    nunjucks configurado retornando res.render index

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
+server.set("view engine", "html");
+
+nunjucks.configure("views", {
+    express:server
+});
+
 server.get("/", function(req, res) {
-    return res.send("Hello World! Jeff and julian");
+    return res.render("index");
 });
 
 server.listen(5000, function() {
